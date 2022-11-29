8b7c459bf87a877e35b85adaecc9a289618b11a6
alexmayberry <alexclimbs@gmail.com>
2022-11-29 15:35:04 -0800

    added ratings to projects, restsructured project photo file system

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,18 +1,25 @@
+// other data imports
 import tools from "./toolsData.json"
-import codesignPlaceholderImage from "../images/placeholder-small.jpg";
-import auxNudgeLarge from "../images/aux-nudge-large.PNG";
-import communityVisionGif from "../images/CommunityVisiondemo.gif"
 
+// image imports
+import placeholderImage from "../images/placeholder-small.jpg";
+import auxNudgeImage from "./aux-nudge/aux-nudge-large.PNG";
+
+// gif imports
+import communityVisionGif from "./community-vision/CommunityVisiondemo.gif"
+
+// projects object
 export const projects = [
     {
         id: 1,
-        name: "CommunityVision",
+        name: "WhoGot?",
         description: "Fullstack web app designed to facilitate and organize community driven urban design proposals.",
         livePage: 'https://heroku.com',
         repo: "https://github.com/alexmayberry/community-vision",
         gif: "gif",
-        image: codesignPlaceholderImage,
-        tools: [tools.react, tools.mongoDB, tools.GraphQL, tools.apollo, tools.mapbox, ]
+        image: placeholderImage,
+        tools: [tools.html5, tools.bulma, tools.heroku, tools.javascript, tools.mysql, tools.mongoDB, tools.handlebars, ],
+        rating: 1
     },
     {
         id: 2,
@@ -21,8 +28,9 @@ export const projects = [
         livePage: 'https://ccaitano.github.io/aux-nudge/',
         repo: "https://github.com/alexmayberry/aux-nudge",
         gif: "gif",
-        image: auxNudgeLarge,
-        tools: [tools.html5, tools.bulma, tools.heroku, tools.javascript, tools.handlebars, ]
+        image: auxNudgeImage,
+        tools: [tools.html5, tools.bulma, tools.heroku, tools.javascript, tools.handlebars, ],
+        rating: 3
     },
     {
         id: 3,
@@ -32,6 +40,7 @@ export const projects = [
         repo: "https://github.com/alexmayberry/community-vision",
         gif: '',
         image: communityVisionGif,
-        tools: [tools.node, tools.heroku, tools.javascript, tools.react, tools.GraphQL, tools.apollo, tools.mongoDB ]
+        tools: [tools.node, tools.heroku, tools.javascript, tools.react, tools.GraphQL, tools.apollo, tools.mongoDB ],
+        rating: 4
     },
 ];
